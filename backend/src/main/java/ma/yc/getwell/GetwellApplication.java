8866cdea9c08ec3a5918c134b771fd409bc34378
devssin <yassin2000ziko@gmail.com>
2024-04-19 11:23:52 +0100

    initial commit

---
+++ backend/src/main/java/ma/yc/getwell/GetwellApplication.java
@@ -0,0 +1,13 @@
+package ma.yc.getwell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetwellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetwellApplication.class, args);
+	}
+
+}
